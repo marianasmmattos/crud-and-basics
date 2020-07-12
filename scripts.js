a43b1183a374710599d9c30561034867c6f10d12
Mariana Mattos <marianasmmattos@gmail.com>
2020-07-12 19:57:35 -0300

    bootcamp-fullstack

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 window.addEventListener('load', start);
+
+function changeColor () {
+    const red = document.getElementById('rangeRed').value;
+    const green = document.getElementById('rangeGreen').value;
+    const blue = document.getElementById('rangeBlue').value;
+    var color = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+    document.body.style.backgroundColor = color;
+    document.getElementById('colorOutput').innerHTML = ':' + color;
+}
+
+document.getElementById('rangeRed').addEventListener('input', changeColor);
+document.getElementById('rangeGreen').addEventListener('input', changeColor);
+document.getElementById('rangeBlue').addEventListener('input', changeColor);
+
 var globalNames = ['um', 'dois', 'três', 'quatro'];
 var inputName = document.getElementById('inputName');
 var isEditing = false;
