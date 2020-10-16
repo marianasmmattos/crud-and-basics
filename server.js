62b51410940dd516dfed239016349413db59f0cb
Mariana Mattos <marianasmmattos@gmail.com>
2020-10-16 10:42:42 -0300

    reorganizando pastas & código

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//-------------------só é necessário pela megasena------------------//
-
 var express = require('express');
 var cors = require('cors');
 var app = express();
@@ -10,38 +8,4 @@ app.get('/', function(request, response){
     response.json({numbers: lotteryNumbers});
 });
 
-var lotteryNumbers = [];
-
-function getNumber(from, to){
-    return (Math.max(Math.ceil(Math.random() * 60)));
-}
-
-function getLotteryNumbers() {
-    lotteryNumbers = [];
-        
-    while(lotteryNumbers.length < 6) {
-        var newNumber = getNumber(1, 60);
-        var insertNumber = true;
-    
-    for(var i =0; i < lotteryNumbers.length; i++){
-        var currentNumber = lotteryNumbers[i];
-
-        if(newNumber === currentNumber){
-            var insertNumber = false;
-            break;
-        }
-    }
-    
-    if(insertNumber) {
-        lotteryNumbers.push(newNumber);
-     }
-    }
-    
-    lotteryNumbers.sort(function(a,b){
-        return a - b;
-    })
-}
-
 app.listen(3000);
-
-// -----------------------------------------------------------------//
